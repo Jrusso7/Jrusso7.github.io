d4f22e101a31be75abeec84e2a45c364d21ad235
Jrusso7 <82895155+Jrusso7@users.noreply.github.com>
2024-07-23 13:44:17 -0400

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('light').addEventListener('click', function() {
-    document.body.classList.remove('dark-mode');
-});
-
-document.getElementById('dark').addEventListener('click', function() {
-    document.body.classList.add('dark-mode');
-});
