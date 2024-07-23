d05c35befe6ca69b9f408c2c5ad787f838cd6ead
Jrusso7 <82895155+Jrusso7@users.noreply.github.com>
2024-07-23 13:26:48 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('light').addEventListener('click', function() {
+    document.body.classList.remove('dark-mode');
+});
+
+document.getElementById('dark').addEventListener('click', function() {
+    document.body.classList.add('dark-mode');
+});
